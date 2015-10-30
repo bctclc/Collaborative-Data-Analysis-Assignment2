9702ea7a777dc097c940a446c08d436440f3a92e
bctclc <cl1007@georgetown.edu>
2015-10-30 16:25:03 +0100

    final dataset

--- datasets/data_cleanup.R
+++ datasets/data_cleanup.R
@@ -15,5 +15,27 @@ data14 <- subset(year14,
 year15 <- read.dta("f1502_mec.dta")
 data15 <- subset(year15, select= -Intro4)
 
+# make column names the same 
+colnames(data14)[which(names(data14) == "highed4")] <- "HighEd4"
+colnames(data14)[which(names(data14) == "NatldE1")] <- "NatID1"
+colnames(data14)[which(names(data14) == "NatldE2")] <- "NatID2"
+colnames(data14)[which(names(data14) == "NatldE3")] <- "NatID3"
+colnames(data14)[which(names(data14) == "NatldE4")] <- "NatID4"
+colnames(data14)[which(names(data14) == "NatldE5")] <- "NatID5"
+colnames(data14)[which(names(data14) == "NatldE6")] <- "NatID6"
+
+
 # combine the two datasets
 data1415 <- rbind(data14, data15)
+
+# drop unwanted variables
+finaldata <- subset(data1415, select= c(MEC_6, MEC_7M01, MEC_7M02, MEC_7M03,
+            MEC_7M04, MEC_7M05, MEC_7M06, MEC_7M07, MEC_7M08,MEC_7M09, MEC_7M10,
+            MEC_8M1, MEC_8M2, MEC_8M3, MEC_10M01, MEC_10M02, MEC_10M03, MEC_10M04,
+            MEC_10M05,MEC_10M06, MEC_10M07, MEC_10M08, MEC_10M09, MEC_10M10, 
+            MEC_11M1, MEC_11M2, MEC_11M3, RSEX, RAGE, GorA, DVHsize, Respmar, CAR,
+            HighEd1, IllLim, DVILO3a, FtPtWk, NSECAC3, sumgross, NumDepCh, MEC_1,
+            MEC_3))
+
+# save the dataset
+save(finaldata, file="clean_dataset.rda")
