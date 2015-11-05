3a0d8b9d9150194125c5ac2ed4ce96ee6d786beb
magaranoriko <norikomagara@gmail.com>
2015-11-05 18:07:34 +0100

    updated logit models

--- datasets/LogitReg.R
+++ datasets/LogitReg.R
@@ -1,4 +1,4 @@
-### logit model from class 10/30 ###
+### logit model for assignment 3 ###
 ### Claire & Noriko ###
 
 library(stargazer)
@@ -25,54 +25,129 @@ finaldata$inc1000 <- finaldata$income/1000
 finaldata$licence[finaldata$MEC_1<=2] <- 1
 finaldata$licence[finaldata$MEC_1==3] <- 0
 
+# travel everyday
+finaldata$travfreq[finaldata$MEC_3==1] <- 1
+finaldata$travfreq[finaldata$MEC_3>=2 & finaldata$MEC_3<=8] <- 0
+table(finaldata$travfreq)
+
+# degree
+finaldata$degree[finaldata$HighEd1==1] <- 1
+finaldata$degree[finaldata$HighEd1>=2 & finaldata$HighEd1<=8] <- 0
+table(finaldata$degree)
+
 
 ### step-wise logistic regression ###
-# Estimate model-1 (AGE & SEX)
-Logit1 <- glm(EVinterest ~ RAGE + Male,
+# Estimate model-1 (AGE & SEX & INCOME & Education & LICENCE)
+L1 <- glm(EVinterest ~ RAGE + Male + inc1000 + degree + licence,
               data = finaldata, family = 'binomial')
-summary(Logit1)
+summary(L1)
 
-# Estimate model-2 (AGE & SEX & INCOME)
-Logit2 <- glm(EVinterest ~ RAGE + Male + inc1000,
-              data = finaldata, family = 'binomial')
-summary(Logit2)
+# Estimate model-2 (AGE & SEX & INCOME & Education & LICENCE & Employment)
+L2 <- glm(EVinterest ~ RAGE + Male + inc1000 + degree + licence + as.factor(DVILO3a),
+          data = finaldata, family = 'binomial')
+summary(L2)
 
-# Estimate model-3 (AGE & SEX & INCOME & LICENCE)
-Logit3 <- glm(EVinterest ~ RAGE + Male + inc1000 + licence,
-              data = finaldata, family = 'binomial')
-summary(Logit3)
+# Estimate model-3 (AGE & SEX & INCOME & Education & LICENCE & Employment & HHsize 
+#                   & Child)
+L3 <- glm(EVinterest ~ RAGE + Male + inc1000 + degree + licence + as.factor(DVILO3a) 
+          + DVHsize + NumDepCh,
+          data = finaldata, family = 'binomial')
+summary(L3)
+
+# Estimate model-4 (AGE & SEX & INCOME & Education & LICENCE & Employment & HHsize 
+#                   & Child & travel freq)
+L4 <- glm(EVinterest ~ RAGE + Male + inc1000 + degree + licence + as.factor(DVILO3a) 
+          + DVHsize + NumDepCh + travfreq,
+          data = finaldata, family = 'binomial')
+summary(L4)
+
+# Estimate model-5 (AGE & SEX & INCOME & Education & LICENCE & Employment & HHsize 
+#                   & Child & travel freq & REGION)
+L5 <- glm(EVinterest ~ RAGE + Male + inc1000 + degree + licence + as.factor(DVILO3a) 
+          + DVHsize + NumDepCh + travfreq + as.factor(GorA),
+          data = finaldata, family = 'binomial')
+summary(L5)
+
+# Estimate model-6 (AGE & SEX & INCOME & Education & LICENCE & Employment & HHsize 
+#                   & Child & travel freq & REGION & work status)
+L6 <- glm(EVinterest ~ RAGE + Male + inc1000 + degree + licence + as.factor(DVILO3a) 
+          + DVHsize + NumDepCh + travfreq + as.factor(GorA) + as.factor(FtPtWk),
+          data = finaldata, family = 'binomial')
+summary(L6)
+
+# Estimate model-7 (AGE & SEX & INCOME & Education & LICENCE & Employment & HHsize 
+#                   & Child & travel freq & REGION & marital)
+L7 <- glm(EVinterest ~ RAGE + Male + inc1000 + degree + licence + as.factor(DVILO3a) 
+          + DVHsize + NumDepCh + travfreq + as.factor(GorA) + as.factor(Respmar),
+          data = finaldata, family = 'binomial')
+summary(L7)
+
+# Estimate model-8 (AGE & SEX & INCOME & Education & LICENCE & Employment & HHsize 
+#                   & Child & travel freq & REGION & marital & car abailability)
+L8 <- glm(EVinterest ~ RAGE + Male + inc1000 + degree + licence + as.factor(DVILO3a) 
+          + DVHsize + NumDepCh + travfreq + as.factor(GorA) + as.factor(Respmar) + CAR,
+          data = finaldata, family = 'binomial')
+summary(L8)
+
+# Estimate model-9 (AGE & SEX & INCOME & Education & LICENCE & Employment & HHsize 
+#                   & Child & travel freq & REGION & marital & car abailability & Disability)
+L9 <- glm(EVinterest ~ RAGE + Male + inc1000 + degree + licence + as.factor(DVILO3a) 
+          + DVHsize + NumDepCh + travfreq + as.factor(GorA) + as.factor(Respmar) + CAR 
+          + as.factor(IllLim),
+          data = finaldata, family = 'binomial')
+summary(L9)
 
 ### displaying the results ###
 # Create cleaner covariate labels
-labels <- c('Age', 'Male', 'Income (in 1000 GBP)', 'Valid licence', '(Intercept)') 
+labels <- c('Age', 'Male', 'Income (in 1000 GBP)', 'College degree', 'Valid licence', 
+            'Unemployed', 'Economically Inactive', 'Household size', 'Dependen Child', 
+            'Travel everyday', 'North West', 'Yorkshire and the Humber', 'East Midlands', 
+            'West Midrands', 'East of England', 'London', 'South East', 'South West', 
+            'Wales', 'Scotland', 'Part-time', 'Married', 'Married-separated', 'Divorced', 
+            'Widowed', 'Car', 'No Illness/Disability', '(Intercept)') 
 
-stargazer::stargazer(Logit1, Logit2, Logit3, covariate.labels = labels,
+stargazer::stargazer(L1, L2, L3, L4, L5, L6, L7, L8, L9, covariate.labels = labels,
                      title = 'Interests in EVs',
-                     digits = 2, type = 'html')
-# the output html can be previewed online for example here: http://www.onlinehtmleditor.net/
+                     digits = 2, type = 'text')
+
+
+# Selected variables? (AGE & MALE & INCOME & DEGREE & LICENCE & MARITAL)
+LP <- glm(EVinterest ~ RAGE + Male + inc1000 + degree + licence + as.factor(Respmar),
+          data = finaldata, family = 'binomial')
+labelsP <- c('Age', 'Male', 'Income (in 1000 GBP)', 'College degree', 'Valid licence', 
+             'Married', 'Married-separated', 'Divorced', 'Widowed', '(Intercept)')
+stargazer(LP, covariate.labels = labelsP,
+          title = 'Interests in EVs',
+          digits = 2, type = 'text')
+
 
 # predicted probabilities
-fitted1 <- with(finaldata,
+fittedP <- with(finaldata,
                 data.frame(RAGE = mean(RAGE),
                            inc1000 = mean(inc1000, na.rm=TRUE),
                            Male = c(0,0,1,1),
-                           licence = c(0,1,0,1)))
+                           licence = c(0,1,0,1),
+                           degree = 1,
+                           Respmar = 1))
 
-fitted1$predicted <- predict(Logit3, newdata = fitted1,
-                            type = 'response')
+fittedP$predicted <- predict(LP, newdata = fittedP,
+                             type = 'response')
 
-kable(fitted1, align = 'c', digits = 2,
+kable(fittedP, align = 'c', digits = 2,
       caption = 'Predicted Probabilities for Fitted Values')
 
 
 ### Zelig plot
 # seems like 'sim' from Zelig doesn't work if any of the variables have missing values.
 # create another subset EVINTEREST without missings
-EVINTEREST <- subset(finaldata, select = c(EVinterest, RAGE, Male, inc1000, licence))
+EVINTEREST <- subset(finaldata, select = c(EVinterest, RAGE, Male, inc1000, degree, 
+                                           licence, Respmar))
 EVINTEREST <- na.omit(EVINTEREST)
 
-Z1 <- zelig(EVinterest ~ RAGE + Male + inc1000 + licence, cite = FALSE,
-              data = EVINTEREST, model = 'logit')
-setZ1 <- setx(Z1, RAGE = 20:80)
-simZ1 <- sim(Z1, x = setZ1)
-plot(simZ1)
+# seems like as.factor() cannot be used in zelig...
+ZP <- zelig(EVinterest ~ RAGE + Male + inc1000 + degree + licence, 
+            cite = FALSE, data = EVINTEREST, model = 'logit')
+setZP <- setx(ZP, RAGE = 20:80)
+simZP <- sim(ZP, x = setZP)
+plot(simZP)
+
