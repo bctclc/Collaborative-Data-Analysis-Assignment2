ef910ed65945906a731605d98c3fd416e2feec9f
magaranoriko <norikomagara@gmail.com>
2015-11-13 00:08:28 +0100

    citation

--- Assignment3/packagetoBibtex.R
+++ Assignment3/packagetoBibtex.R
@@ -1,5 +1,7 @@
 # set working directory (change this line to your repository location)
 setwd("C:/Users/noriko/Desktop/Collaborative-Data-Analysis-Assignment2/Assignment3")
 
-packs <- c("repmis", "foreign", "dplyr", "stargazer", "knitr", "Zelig", "rms", "markdown")
+library(repmis)
+
+packs <- c("repmis", "foreign", "dplyr", "stargazer", "knitr", "Zelig", "rms", "markdown", "captioner")
 repmis::LoadandCite(packs, file="Package_Citation.bib")
