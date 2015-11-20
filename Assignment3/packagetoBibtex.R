56a2ba97c469b568ef4253800c185b6fc8f7d3c1
magaranoriko <norikomagara@gmail.com>
2015-11-20 12:54:55 +0100

    delete

--- Assignment3/packagetoBibtex.R
+++
@@ -1,7 +0,0 @@
-# set working directory (change this line to your repository location)
-setwd("E:\\—ßœ∞œ‡πÿ\\R\\Collaborative-Data-Analysis-Assignment2\\Assignment3")
-
-library(repmis)
-
-packs <- c("repmis", "foreign", "dplyr", "stargazer", "knitr", "Zelig", "rms", "markdown", "captioner")
-repmis::LoadandCite(packs, file="Package_Citation.bib")
