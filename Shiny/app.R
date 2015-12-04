87a0735543bd847e6f8306f13440dcaa28f23210
magaranoriko <norikomagara@gmail.com>
2015-12-04 17:26:14 +0100

    shiny-still don't work

--- Shiny/app.R
+++ Shiny/app.R
@@ -13,10 +13,9 @@ library(proj4)
 setwd("C:/Users/noriko/Desktop/Collaborative-Data-Analysis-Assignment2/Shiny")
 
 ### the dataframe...
+load("data4web.rda")
 
 ### Prepare Shapefiles
-# RegionSHP <- readOGR(dsn = "./Region", layer = "RGN_DEC_2014_GB_BGC")
-
 # shapefile
 tmp_dir = tempdir()
 url_data = "https://geoportal.statistics.gov.uk/Docs/Boundaries/Regions_(GB)_2014_Boundaries_(Generalised_Clipped).zip"
@@ -28,18 +27,8 @@ gor=readShapeSpatial(sprintf('%s/RGN_DEC_2014_GB_BGC.shp', tmp_dir))
 # object ID to merge with EV interest data
 gor@data$OID <- c(1,2,3,4,5,6,7,8,9,11,10)
 
+RegionDF = data.frame(gor@data, RegionDF[match(gor@data$OID, RegionDF$OID),])
 
-### Independent variables
-Data <- read.csv("Data/data.csv")
-Shapes_krs$numb.ini <- (Data$numb.ini-min(Data$numb.ini))/(max(Data$numb.ini)-min(Data$numb.ini))
-Shapes_krs$unemployment <- (Data$unemployment-min(Data$unemployment, na.rm = TRUE))/(max(Data$unemployment, na.rm = TRUE)-min(Data$unemployment, na.rm = TRUE))
-Shapes_krs$gender.ratio <- (Data$gender.ratio-min(Data$gender.ratio))/(max(Data$gender.ratio)-min(Data$gender.ratio))
-Shapes_krs$young.per <- (Data$young.per-min(Data$young.per))/(max(Data$young.per)-min(Data$young.per))
-Shapes_krs$abitur.per <- (Data$abitur.per-min(Data$abitur.per, na.rm = TRUE))/(max(Data$abitur.per, na.rm = TRUE)-min(Data$abitur.per, na.rm = TRUE))
-Shapes_krs$GDP.cap <- (Data$GDP.cap-min(Data$GDP.cap, na.rm = TRUE))/(max(Data$GDP.cap, na.rm = TRUE)-min(Data$GDP.cap, na.rm = TRUE))
-Shapes_krs$pop.dens <- (Data$pop.dens-min(Data$pop.dens, na.rm = TRUE))/(max(Data$pop.dens, na.rm = TRUE)-min(Data$pop.dens, na.rm = TRUE))
-Shapes_krs$east <- (Data$east-min(Data$east))/(max(Data$east)-min(Data$east))
-Shapes_krs$refugee.ratio <- (Data$refugee.ratio-min(Data$refugee.ratio, na.rm = TRUE))/(max(Data$refugee.ratio, na.rm = TRUE)-min(Data$refugee.ratio, na.rm = TRUE))
 
 ### Set color (for dots?)
 r_colors <- rgb(t(col2rgb(colors()) / 255))
@@ -57,15 +46,23 @@ ui <- fluidPage(
   
   selectInput("data", "Data per district:",
               choices = 
-                c("Unemployment ratio",
-                  "Gender ratio",
-                  "Young people ratio",
-                  "Abitur ratio",
-                  "GDP per capita",
-                  "Population density",
-                  "East-West",
-                  "Refugee ratio",
-                  "Refugee initiatives"
+                c("Females",
+                  "Males",
+                  "Young People",
+                  "Middle-Age People",
+                  "Old-Age People",
+                  "Low-Income People",
+                  "Lower-Middle Income People",
+                  "Higher-Middle Income People",
+                  "High-Income People",
+                  "College Graduates",
+                  "Non-College Graduates",
+                  "Having Driver's Licence",
+                  "Not Having Driver's Licence",
+                  "People with No Car",
+                  "People with One Car",
+                  "People with Two Cars",
+                  "People with Three or More Cars"
                 )),
   
   leafletOutput("mymap")
@@ -77,21 +74,25 @@ server <- function(input, output, session) {
   
   datasetInput <- reactive({
     switch(input$data,
-           "Refugee initiatives" = Shapes_krs$numb.ini,
-           "Unemployment ratio" = Shapes_krs$unemployment,
-           "Gender ratio" = Shapes_krs$gender.ratio,
-           "Young people ratio" = Shapes_krs$young.per,
-           "Abitur ratio" = Shapes_krs$abitur.per,
-           "GDP per capita" = Shapes_krs$GDP.cap,
-           "Population density" = Shapes_krs$pop.dens,
-           "East-West" = Shapes_krs$east,
-           "Refugee ratio" = Shapes_krs$refugee.ratio
+           "Females"=RegionDF$IntFemales,
+           "Males"=RegionDF$IntMales,
+           "Young People"=RegionDF$IntYoung,
+           "Middle-Age People"=RegionDF$IntMiddleage,
+           "Old-Age People"=RegionDF$IntOldage,
+           "Low-Income People"=RegionDF$IntLow,
+           "Lower-Middle Income People"=RegionDF$IntLowmid,
+           "Higher-Middle Income People"=RegionDF$IntHighmid,
+           "High-Income People"=RegionDF$IntHigh,
+           "College Graduates"=RegionDF$IntCollege,
+           "Non-College Graduates"=RegionDF$IntNocollege,
+           "Having Driver's Licence"=RegionDF$IntLicence,
+           "Not Having Driver's Licence"=RegionDF$IntNolicence,
+           "People with No Car"=RegionDF$IntNocar,
+           "People with One Car"=RegionDF$IntOnecar,
+           "People with Two Cars"=RegionDF$IntTwocars,
+           "People with Three or More Cars"=RegionDF$IntThreecars
     )
   })
-  
-  points <- eventReactive(input$recalc, {
-    cbind(data$Longitude, data$Latitude)
-  }, ignoreNULL = FALSE)
   
   pop <- eventReactive(input$recalc, {
     paste(data$Names)
@@ -100,10 +101,10 @@ server <- function(input, output, session) {
   output$mymap <- renderLeaflet({
     leaflet() %>% 
       addTiles() %>% 
-      addPolygons(data=Shapes_krs, weight=2, fillOpacity = 0.8, 
+      addPolygons(data=RegionDF, weight=2, fillOpacity = 0.8, 
                   smoothFactor = 0.5, 
                   color = ~colorBin("YlOrBr", bins = c(0,0.1,0.2,0.3,0.5,0.7,0.9,1), pretty = TRUE,
-                                    na.color = "white", Shapes_krs)
+                                    na.color = "white", RegionDF)
                   (datasetInput() )) %>%
       
       addMarkers(data=points(), popup= paste(data$Names)) 
