df0b9a08d77f84455ba0b91bb2db9b7812c3a900
magaranoriko <norikomagara@gmail.com>
2015-12-10 13:39:33 +0100

    updated app & a bit of organization

---
+++ Shiny/App2Web.R
@@ -0,0 +1,6 @@
+# deploy app to website
+
+setwd('C:/Users/noriko/Desktop/Collaborative-Data-Analysis-Assignment2/Shiny')
+
+library(shinyapps)
+deployApp('C:/Users/noriko/Desktop/Collaborative-Data-Analysis-Assignment2/Shiny')
